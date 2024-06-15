e289836922a9ffcc599a846815463140ffb610d2
ASKOVICHSENDWICH <sanzharsanzhar1010@gmail.com>
2024-06-15 09:21:17 +0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
+document.getElementById('tasksTab').addEventListener('click', function() {
+    document.getElementById('mainContent').style.display = 'none';
+    document.getElementById('tasksContent').style.display = 'block';
+    document.getElementById('referralContent').style.display = 'none';
+});
+
+document.getElementById('mainTab').addEventListener('click', function() {
+    document.getElementById('mainContent').style.display = 'block';
+    document.getElementById('tasksContent').style.display = 'none';
+    document.getElementById('referralContent').style.display = 'none';
+});
+
+document.getElementById('referralTab').addEventListener('click', function() {
+    document.getElementById('mainContent').style.display = 'none';
+    document.getElementById('tasksContent').style.display = 'none';
+    document.getElementById('referralContent').style.display = 'block';
+});
+
 let mainScore = 0;
 let earningScore = 0;
 let isFarming = false;
+let farmingInterval;
 const earningsPerSecond = 2; // Количество очков в секунду
 const maxEarningScore = 60; // Максимальное количество очков заработка
 const updateInterval = 1000; // 1 секунда
@@ -26,7 +45,7 @@ document.getElementById('farmButton').addEventListener('click', function() {
     if (!isFarming) {
         isFarming = true;
         this.disabled = true; // Делаем кнопку "ФАРМИТЬ" неактивной
-        const farmingInterval = setInterval(() => {
+        farmingInterval = setInterval(() => {
             if (earningScore < maxEarningScore) {
                 earningScore += earningsPerSecond;
                 if (earningScore > maxEarningScore) {
@@ -50,6 +69,7 @@ document.getElementById('collectButton').addEventListener('click', function() {
         document.getElementById('farmButton').disabled = false; // Делаем кнопку "ФАРМИТЬ" активной
         this.disabled = true; // Делаем кнопку "СОБРАТЬ" неактивной
         isFarming = false; // Сбрасываем состояние фарминга
+        clearInterval(farmingInterval); // Останавливаем текущий интервал фарминга
     }
 });
 
